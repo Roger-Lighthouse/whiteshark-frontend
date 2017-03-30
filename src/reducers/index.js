2922f247671786202ad53562fdc86efed62e859e
Roger-Lighthouse <roger@whiteshark.ca>
2017-03-30 13:27:00 -0400

    modified inex reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,28 @@
 import { combineReducers } from 'redux'
 import jobs from './jobs'
+import jobReducer from './jobs'
 import visibilityFilter from './visibilityFilter'
 
 const jobsApp = combineReducers({
+  job: jobReducer,
+
   jobs,
   visibilityFilter
 })
 
 export default jobsApp
+
+
+
+
+// import { routerReducer } from 'react-router-redux'
+// import counterReducer from './counter'
+// import myjobsReducer from './myjobs'
+
+// let rootReducer = combineReducers({
+//     myjobs: myjobsReducer,
+//     counter: counterReducer,
+//     router: routerReducer,
+// })
+
+// export default rootReducer
